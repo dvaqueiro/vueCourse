fd0a7d064743b762a28a5758dc71673980846981
dvaqueiro <dvaqueiro@boardfy.com>
2019-06-08 15:48:21 +0200

    Exercise 10 solution

--- Exercise_10/src/App.vue
+++ Exercise_10/src/App.vue
@@ -3,6 +3,9 @@
         <div class="row">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <h1>Directives Exercise</h1>
+                <button class="btn btn-default" v-myon:click="showAlert">
+                    click here!
+                </button>
                 <!-- Exercise -->
                 <!-- Build a Custom Directive which works like v-on (Listen for Events) -->
 
@@ -12,7 +15,13 @@
 </template>
 
 <script>
-    export default {}
+    export default {
+        methods: {
+            showAlert: function() {
+                alert("Button clicked");
+            }
+        }
+    }
 </script>
 
 <style>
--- Exercise_10/src/main.js
+++ Exercise_10/src/main.js
@@ -1,6 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+Vue.directive('myon', {
+  bind(el, binding, vnode) {
+    el.addEventListener(binding.arg, binding.value); 
+  }
+});
+
 new Vue({
   el: '#app',
   render: h => h(App)
