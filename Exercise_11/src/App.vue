e334a881bf715d8177c81283a5ecfaf273c82014
dvaqueiro <dvaqueiro@boardfy.com>
2019-06-09 12:59:48 +0200

    Exercise 11, step 2: global filter

--- Exercise_11/src/App.vue
+++ Exercise_11/src/App.vue
@@ -5,10 +5,7 @@
                 <h1>Filters & Mixins</h1>
                 <input type="text" v-model="someText" name="someText" id="someText"/>
                 <p>Reverse text filter: {{ someText | reverseText }}</p>
-
-                <!-- Exercise 2 -->
-                <!-- Build a global Filter which counts the length of a word and it appends it -->
-                <!-- Like this: "Test" => Gets Filtered to => "Test (4)" -->
+                <p>Add count: {{ someText | add-count }}</p>
 
                 <!-- Exercise 3 -->
                 <!-- Do the same as in Exercises 1 & 2, now with Computed Properties -->
